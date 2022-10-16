eba45ee1cec8163db901095450c84f3f6859e833
heyraeh <heyraeh@gmail.com>
2022-10-15 18:29:03 -0700

    Updates

---
+++ precache-manifest.c9872df9d9e70b4acd787dbbf079ccf7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "bf25374a2e47f806fb08",
+    "url": "/heyraeh-robofriends/static/css/main.cb090ff5.chunk.css"
+  },
+  {
+    "revision": "bf25374a2e47f806fb08",
+    "url": "/heyraeh-robofriends/static/js/main.d18e34e1.chunk.js"
+  },
+  {
+    "revision": "2bcbf0434732185887d8",
+    "url": "/heyraeh-robofriends/static/js/runtime~main.d03f662a.js"
+  },
+  {
+    "revision": "60851fa55d07541e3eae",
+    "url": "/heyraeh-robofriends/static/css/2.4576162c.chunk.css"
+  },
+  {
+    "revision": "60851fa55d07541e3eae",
+    "url": "/heyraeh-robofriends/static/js/2.3a99004e.chunk.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/heyraeh-robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "300b052dac0f6e10144210c727d6affd",
+    "url": "/heyraeh-robofriends/index.html"
+  }
+];
